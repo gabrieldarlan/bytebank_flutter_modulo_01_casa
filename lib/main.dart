a5a202286d3d91028635f5f44d8338a3b9e9e02e
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-02-25 02:47:20 -0300

    aula 04 - 01 Extraindo widgets flexiveis

--- lib/main.dart
+++ lib/main.dart
@@ -27,34 +27,16 @@ class FormularioTransferencia extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _controladorCampoNumeroConta,
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                labelText: 'Número da conta',
-                hintText: '0000',
-              ),
-              keyboardType: TextInputType.number,
-            ),
+          Editor(
+            controlador: _controladorCampoNumeroConta,
+            dica: '0000',
+            rotulo: 'Número da conta',
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: _controladorCampoValor,
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                labelText: 'Valor',
-                hintText: '0.00',
-                icon: Icon(Icons.monetization_on),
-              ),
-              keyboardType: TextInputType.number,
-            ),
+          Editor(
+            icone: Icons.monetization_on,
+            controlador: _controladorCampoValor,
+            dica: '0.00',
+            rotulo: 'Valor',
           ),
           RaisedButton(
             child: Text('Confirma'),
@@ -70,6 +52,39 @@ class FormularioTransferencia extends StatelessWidget {
             },
           )
         ],
+      ),
+    );
+  }
+}
+
+class Editor extends StatelessWidget {
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData icone;
+
+  const Editor({
+    this.controlador,
+    this.rotulo,
+    this.dica,
+    this.icone,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(
+          fontSize: 24.0,
+        ),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone) : null,
+          labelText: rotulo,
+          hintText: dica,
+        ),
+        keyboardType: TextInputType.number,
       ),
     );
   }
